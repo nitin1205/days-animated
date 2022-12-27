9d7ffebd4befc47513dd3ebb78bb7973111a830a
Nitin Singh <vortechcorp@gmail.com>
2022-12-27 22:56:10 +0530

    added page transition animation for fashion page and added scroll animation for fashion page

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 
 function animateSlides() {
   
@@ -105,10 +106,126 @@ function navToggle(e) {
   }
 }
 
+
+// page transition using barba
+// barba.init({
+//   views: [
+//     {
+//       namespace: "home",
+//       beforeEnter() {
+//         animateSlides();
+//       },
+//       beforeLeave() {
+//         slideScene.destroy();
+//         pageScene.destroy();
+//         controller.destroy();
+//       }
+//     },
+//     {
+//       namespace: "fashion"
+//     }
+//   ],
+//   transitions: [
+//     {
+//       leave({current, next}) {
+//         let done = this.async();
+
+
+//         const timeline = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+//         timeline.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0, onComplete: done  }); 
+//       },
+//       enter(current, next) {
+//         let done = this.async();
+//         const timeline = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+//         timeline.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1, onComplete: done });
+//       }
+//     }
+//   ]
+// });
+
+const logo = document.querySelector("#logo");
+barba.init({
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        animateSlides();
+        logo.href = './index.html';
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        pageScene.destroy();
+        controller.destroy();
+      }
+    },
+    {
+      namespace: "fashion",
+      beforeEnter() {
+        logo.href = '../index.html';
+        detailAnimation();
+        gsap.fromTo('.nav-header', 1, { y: '100%'}, { y: '0%', ease: 'power2.inOut' })
+
+      },
+      beforeLeave() {
+        controller.destroy();
+        detailScene.destroy();
+      }
+    }
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async();
+        //An Animation
+        const timeline = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        timeline.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        timeline.fromTo('.swipe', 0.75, {x: '-100%'}, {x: '0%', onComplete: done}, '-=0.5');
+      },
+      enter({ current, next }) {
+        let done = this.async();
+        //Scroll to the top
+        window.scrollTo(0, 0);
+        //An Animation
+        const timeline = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        timeline.fromTo('.swipe', 1, {x: '0%'}, {x: '100%', stagger: 0.25, onComplete: done});
+        timeline.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+      }
+    }
+  ]
+});
+
+function detailAnimation() {
+  controller = new ScrollMagic.Controller();
+  const slides = document.querySelectorAll('.detail-slide');
+
+  slides.forEach((slide, index, slides) => {
+    const slideTl = gsap.timeline({ defaults: { duration:1 } });
+    let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+    const nextImg = nextSlide.querySelector('img');
+    slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, '-=1');
+    slideTl.fromTo(nextImg, { x: '50%' }, { x: '0%' });
+
+    detailScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: '100%',
+      triggerHook: 0
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: 'white',
+        colorTrigger: 'white',
+        name: 'detailscene'
+      })
+      .addTo(controller)
+  });
+}
+
+
 burger.addEventListener("click", navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activecursor);
 
-animateSlides();
 
 
